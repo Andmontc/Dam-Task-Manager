ab20c6c07d18e1add87fa53349045965b16d5f76
Andmontc <oscarandresmontes@gmail.com>
2021-01-12 11:25:20 -0500

    UPDATE main index file for hook types context files

---
+++ front/src/types/index.js
@@ -0,0 +1,24 @@
+export const PROJECT_FORM = 'PROJECT_FORM';
+export const GET_PROJECTS = 'GET_PROJECTS';
+export const ADD_PROJECTS = 'ADD_PROJECTS';
+export const VALIDATE_PROJECTS = 'VALIDATE_PROJECTS';
+export const ACTUAL_PROJECT = 'ACTUAL_PROJECT';
+export const DELETE_PROJECT = 'DELETE_PROJECT';
+export const PROJECT_ERROR = 'PROJECT_ERROR';
+
+export const GET_TASKS = 'GET_TASKS';
+export const ADD_TASKS = 'ADD_TASKS';
+export const VALIDATE_TASK = 'VALIDATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const ACTUAL_TASK = 'ACTUAL_TASK';
+export const EDIT_TASK = 'EDIT_TASK';
+
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const HIDE_ALERT = 'HIDE_ALERT';
+
+export const SUCCESS_REGISTER = 'SUCCESS_REGISTER';
+export const ERROR_REGISTER = 'ERROR_REGISTER';
+export const GET_USER = 'GET_USER';
+export const SUCCESS_LOGIN = 'SUCCESS_LOGIN';
+export const ERROR_LOGIN = 'ERROR_LOGIN';
+export const LOG_OUT = 'LOG_OUT';
